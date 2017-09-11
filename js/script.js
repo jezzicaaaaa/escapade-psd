c7df325281b9b8faf6e247112313288d6197fbe2
Jezzica <jezzicamae@gmail.com>
2017-09-11 18:53:49 -0400

    added hamburger nav

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+
+//hide cross on page load
+  $( ".cross" ).hide();
+  $( ".hamburger" ).click( function(e) {
+    $( ".menu" ).toggleClass( "open-nav" );
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+
+  });
+
+  $( ".cross" ).click(function() {
+    $( ".menu" ).toggleClass( "open-nav" );
+    $( ".cross" ).hide();
+    $( ".hamburger" ).show();
+
+  });
+
+});
